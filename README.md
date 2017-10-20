bab5bd954e8bd1ec2f5b9f0e10f48c7c4e23cf22
MissLixf <1090474924@qq.com>
2017-10-20 19:34:05 +0800

    addfiles

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# node
-### this is master
+##global.js
---
+++ global.js
@@ -0,0 +1,17 @@
+// console.log('hello node.js');
+// setTimeout(function(){
+// 	console.log("3s show");
+// },3000)
+// var time=0;
+// var timer=setInterval(function(){
+// 	time+=2;
+// 	if(time>5){
+// 		clearInterval(timer);
+// 	}else{
+// 		console.log(time+"show/3s");
+// 	}
+	
+// },3000);
+
+console.log(__dirname);
+console.log(__filename);
