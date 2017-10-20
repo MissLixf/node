89a10cc9ef13fe8d72728faaf43bc1202195d338
MissLixf <1090474924@qq.com>
2017-10-20 19:44:27 +0800

    addfiles

---
+++ node1/app.js
@@ -0,0 +1,9 @@
+
+var stuff=require("./module");//引入模块
+
+// console.log(stuff.counter(['a','b','c','d']));
+// console.log(stuff.adder(3,5));
+// console.log(stuff.pi);
+
+Stuff=new stuff;
+Stuff.printNextCount();
